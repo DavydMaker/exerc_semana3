b9f59c4d5ad93a1a581f2a3e447237223847a03f
Davyd Maker <davydmaker@gmail.com>
2017-01-14 20:30:44 -0200

    Add files via upload

---
+++ l2_ordenacao.py
@@ -0,0 +1,8 @@
+a = int(input("Digite o número a: "))
+b = int(input("Digite o número b: "))
+c = int(input("Digite o número c: "))
+
+if a <= b and b <= c:
+	print("crescente")
+else:
+	print("não está em ordem crescente")
